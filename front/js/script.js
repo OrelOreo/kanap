6bf6600eec08c4d0c7982863395e3cbe2a83085a
OrelOreo <oreloreopro@gmail.com>
2023-01-11 23:13:34 +0100

    insert products in home page and recover id of product

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,37 @@
-const items = document.querySelector('#items')
-let link = document.querySelector('#items a')
-let article = document.querySelector('article')
-let productName = document.querySelector('.productName')
-let productDescription = document.querySelector('.productDescription')
-let image = document.querySelector('#items img')
-
-
-
 fetch("http://localhost:3000/api/products")
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => genererProducts(data))
 
+function genererProducts(data, id) {
+// Rattachement des éléments
+
+for (let i = 0; i < data.length; i++) {
+    const product = data[i]
+    // Récupération des ID de chaque produits
+    const idProduct = product._id[i]
+    // Récupération de l'élément du DOM qui accueillera les produits
+    const sectionArticles = document.querySelector('.items')
+    // Création d'une balise link dédié à un produit
+    const linkElement = document.createElement('a')
+    linkElement.href = `./product.html?id=${idProduct}`
+    // Création d'une balise dédiée à un produit
+    const articleElement = document.createElement('article')
+    // On crée élément img
+    const imageElement = document.createElement('img')
+    // On crée le nom
+    const nomElement = document.createElement('h3')
+    nomElement.innerText = product.name
+    // On crée le prix
+    const descriptionElement = document.createElement('p')
+    descriptionElement.innerText = product.description
+    // On accède à l'index de la liste pour configurer la source de l'image
+    imageElement.src = data[i].imageUrl
+
+
+    sectionArticles.appendChild(linkElement)
+    linkElement.appendChild(articleElement)
+    articleElement.appendChild(imageElement)
+    articleElement.appendChild(nomElement)
+    articleElement.appendChild(descriptionElement)
+    }
+}
