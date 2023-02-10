f224066de244da7e340d81815e24e8a7c33000fe
OrelOreo <oreloreopro@gmail.com>
2023-02-10 10:50:50 +0100

    fonctionnalité suppression du produit dans panier et début regex pour formulaire

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,13 @@
 const sectionCartItems = document.querySelector('#cart__items')
+const totalPriceElement = document.querySelector('#totalPrice')
+const totalQuantity = document.querySelector('#totalQuantity')
+let totalArticlesPrice = 0
+let totalArticlesQuantity = 0
 
 async function getProductById(artId) {
     return fetch("http://localhost:3000/api/products")
         .then(function (res) {
             return res.json()
-        })
-        .catch((err) => {
-            console.log("err", err)
         })
         .then((response) => {
             for (let i = 0; i < response.length; i++) {
@@ -16,6 +17,9 @@ async function getProductById(artId) {
                 }
             }
         })
+        .catch((err) => {
+            console.log("err", err)
+        })
 }
 
 const products = JSON.parse(localStorage.getItem('products'))
@@ -23,6 +27,9 @@ const products = JSON.parse(localStorage.getItem('products'))
 async function showCart() {
     for (let i = 0; i < products.length; i++) {
         let {price, name, imageUrl, altTxt} = await getProductById(products[i].id)
+        totalArticlesQuantity += parseInt(products[i].inputQuantity)
+        totalArticlesPrice += parseInt(products[i].inputQuantity * price)
+
         let article = `
                         <article class="cart__item" data-id="${products[i].id}" data-color="${products[i].selectColor}">
                             <div class="cart__item__img">
@@ -45,8 +52,68 @@ async function showCart() {
                                 </div>
                             </div>
                         </article>
-        `
+                      `
         sectionCartItems.innerHTML += article
+        totalQuantity.innerText = totalArticlesQuantity
+        totalPriceElement.innerText = totalArticlesPrice
     }
+    // Suppression des articles dans le DOM et également du localStorage
+    const deleteButtons = document.querySelectorAll('.deleteItem')
+    deleteButtons.forEach(deleteButton => {
+    deleteButton.addEventListener('click', (event) => {
+        const articleToDelete = event.target.closest('.cart__item')
+        articleToDelete.remove()
+
+        const products = JSON.parse(localStorage.getItem('products'))
+        // Trouver l'index de l'article dans la liste de produits dans le localStorage
+        const articleIndex = products.findIndex(product => product.id === articleToDelete.dataset.id && product.selectColor === articleToDelete.dataset.selectColor)
+        // Supprimer de cette liste avec .splice, 1er arguement l'index du produit, 2nd argument combien d'élément je supprime
+        products.splice(articleIndex, 1)
+        // Mettre a jour le localStorage avec la nouvelle liste
+        localStorage.setItem('products', JSON.stringify(products))
+    })
+})
 }
+
+
 showCart()
+
+
+
+// const deleteButtons = Array.from(document.querySelectorAll('.deleteItem'))
+// console.log(deleteButtons)
+
+
+
+
+
+
+// FORM REGEX
+
+const validationForm = {
+    firstName: {
+      inputElementForm: document.getElementById("firstName"),
+      // cette expression régulière permet de vérifier que la chaîne de caractères ne contient que des lettres, sans espaces ni autres caractères, et que la chaîne commence et se termine par une lettre.
+      regex: /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/,
+    },
+    lastName: {
+      inputElementForm: document.getElementById("lastName"),
+      // cette expression régulière permet de vérifier que la chaîne de caractères ne contient que des lettres, sans espaces ni autres caractères, et que la chaîne commence et se termine par une lettre.
+      regex: /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/,
+    },
+    address: {
+      inputElementForm: document.getElementById("address"),
+      // cette expression régulière permet de vérifier que la chaîne de caractères commence par un nombre entier compris entre 1 et 99999, suivi d'une séquence de caractères qui peut inclure des lettres, des chiffres, des espaces, des virgules, des apostrophes et des tirets. La chaîne peut également inclure jusqu'à trois séquences supplémentaires de ce type, mais ces séquences sont optionnelles.
+      regex: /^\d{1,5} [A-Za-z0-9\s,'-]{1,30}(?: [A-Za-z\s,'-]+){0,3}$/,
+    },
+    city: {
+      inputElementForm: document.getElementById("city"),
+      // cette expression régulière permet de vérifier que la chaîne de caractères est composée d'un ou plusieurs mots, chaque mot étant composé d'une première lettre majuscule suivie d'une ou plusieurs minuscules. Les mots peuvent être séparés par un espace ou un tiret.
+      regex: /^[A-Z][a-z]+([\s-][A-Z][a-z]+)*$/,
+    },
+    email: {
+      inputElementForm: document.getElementById("email"),
+      // cette expression régulière permet de vérifier que la chaîne de caractères est une adresse e-mail valide, qui commence par un nom d'utilisateur qui peut inclure des lettres, des chiffres, des points, des soulignés, des plus et des moins, suivi du caractère @ et du nom de domaine qui peut inclure des lettres, des chiffres et des points.
+      regex: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    },
+  };
